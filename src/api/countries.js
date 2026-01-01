cea79682fb2cdc63269a6702bc80f0274c3fe7c9
Pranav Aggarwal <pranav.aggarwal2004@gmail.com>
2026-01-02 00:40:29 +0530

    SF-Commit

--- src/api/countries.js
+++ src/api/countries.js
@@ -5,4 +5,5 @@ const BASE_URL = "https://685013d7e7c42cfd17974a33.mockapi.io";
 export const getCountries = async () => {
   const res = await axios.get(`${BASE_URL}/countries`);
   return res.data;
+
 };
